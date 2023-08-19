3c0f930cad5f40f6ab36c2281865043ea02da8af
KiruthickK <kiruthick101@gmail.com>
2023-08-19 06:08:58 +0530

    Game over condition added

--- game.js
+++ game.js
@@ -23,3 +23,45 @@ function isPositionValid(row, col){
     board[row][col] = 'o';
     return true;
 }
+function isGameOver(player){
+    let result = true;
+    // row wise check
+    for(var i=0; i<=max; i++){
+        for(var j = 0; j<= max; j++){
+            if(board[i][j] != player){
+                result = false;
+                break;
+            }
+        }
+    }
+    if(result){
+        return result;
+    }
+    result = true;
+    // coloumn wise check
+    for(var i=0; i<=max; i++){
+        for(var j = 0; j<= max; j++){
+            if(board[j][i] != player){
+                result = false;
+                break;
+            }
+        }
+    }
+    if(result){
+        return result;
+    }
+    //diagonal check
+    //as of now for simplicity manually i have made this, will change to like formula in the future
+    if(board[0][0] == player && board[1][1] == player && board[2][2] == player){
+        return false;
+    }
+    if(board[2][0] == player && board[1][1] == player && board[0][2] == player){
+        return true;
+    }
+    return false;
+}
+function itsComputerMove(){
+    while(true){
+        
+    }
+}
