40460f3bb42f999e2c27a30df8b54e30c6f8ad24
KiruthickK <kiruthick101@gmail.com>
2023-12-04 06:31:48 +0530

    initiated

--- script.js
+++ script.js
@@ -1,61 +1,26 @@
-window.onload = (() => {
-    alert('This game is not developed completely, it have bugs and we are making effort to fix it. Thanks for playing');
-    createActiveListeners();
-    initializeGame();
-})
+window.onload = () => {
+    initialiseTable();
+}
+const user = 'x';
+const bot = 'o';
 const x_mark = `<img src="./Assets/x.png" alt="">`;
 const o_mark = `<img src="./Assets/circle.png" alt="">`;
-async function newGame() {
-    await initializeGame();
-    await createActiveListeners();
-}
-function createActiveListeners() {
-    return new Promise(resolve => {
-        const table = document.querySelector('table');
-        const rows = table.querySelectorAll('tr');
-        for(var i = 0; i<rows.length;i++){
-            const tds = rows[i].querySelectorAll('td');
-            for(var j = 0;j<tds.length;j++){
-                tds[j].id = 'td-'+i+'-'+j;
-                tds[j].innerHTML = '';
-                tds[j].onclick = ((cell) => {
-                    console.log(cell.srcElement.id);
-                    cellClicked(cell.srcElement.id);
-                });
-            }
+function initialiseTable() {
+    const tbody = document.getElementById('table_body');
+    const rows = tbody.getElementsByTagName('tr');
+    for (var i = 0; i < rows.length; i++) {
+        const cols = rows[i].getElementsByTagName('td');
+        rows[i].id = `row-${i}`;
+        for (var j = 0; j < cols.length; j++) {
+            cols[j].id = `r-${i}:c-${j}`;
+            ((i, j) => {
+                cols[j].onclick = () => {
+                    clickHandlerForCell(i, j)
+                }
+            })(i, j);
         }
-        resolve();
-    });
-}
-function rowColExtractionFromId(id){
-    return new Promise(resolve => {
-        let datas = id.split('-');
-        resolve([datas[1], datas[2]]);
-    })
-}
-async function cellClicked(id){
-    var rowcol;
-    await rowColExtractionFromId(id).then(res => {
-        rowcol = res;
-    }).catch(err => {
-        console.log(err);
-    });
-    if(await isPositionValid(+rowcol[0], +rowcol[1], 'x')){
-        document.getElementById(id).innerHTML = x_mark;
-        console.log(board);
-        if(await isGameOver('x')){
-            // document.getElementById(id).innerHTML = x_mark;
-            alert("Yayy!, you won");
-            return;
-        }
-    }else{
-        return;
     }
-    await itsComputerMove(o_mark, id);
-    console.log(board);
-    if(await isGameOver('o')){
-        // document.getElementById(id).innerHTML = o_mark;
-        alert("Hahah I won!");
-        return;
-    }
+}
+async function clickHandlerForCell(row, col) {
+
 }
