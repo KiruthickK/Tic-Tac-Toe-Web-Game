9e126529c8635b276c7e615a39f759ea4761131e
Kiruthick KPIT <kiruthick101@gmail.com>
2024-01-17 07:09:53 +0530

    initiated...

--- script.js
+++ script.js
@@ -1,61 +1,42 @@
-window.onload = (() => {
-    alert('This game is not developed completely, it have bugs and we are making effort to fix it. Thanks for playing');
-    createActiveListeners();
-    initializeGame();
-})
-const x_mark = `<img src="./Assets/x.png" alt="">`;
-const o_mark = `<img src="./Assets/circle.png" alt="">`;
-async function newGame() {
-    await initializeGame();
-    await createActiveListeners();
+var user;
+var bot;
+window.onload = ()=>{
+    let choice = prompt('Type 1 for choosing \'X\' or enter any other number for choosing \'O\'');
+    if(choice == 1){
+        alert('You have chosen "X" to play');
+        user = 'X';
+        bot = 'O';
+    }else{
+        alert('You have chosen "O" to play');
+        user = 'O';
+        bot = 'X';
+    }
+    initialisePlay(user);
 }
-function createActiveListeners() {
-    return new Promise(resolve => {
-        const table = document.querySelector('table');
-        const rows = table.querySelectorAll('tr');
-        for(var i = 0; i<rows.length;i++){
-            const tds = rows[i].querySelectorAll('td');
-            for(var j = 0;j<tds.length;j++){
-                tds[j].id = 'td-'+i+'-'+j;
-                tds[j].innerHTML = '';
-                tds[j].onclick = ((cell) => {
-                    console.log(cell.srcElement.id);
-                    cellClicked(cell.srcElement.id);
-                });
-            }
+let userSymbol;
+let botSymbol;
+async function initialisePlay(user){
+    if(user == 'X'){
+        userSymbol = `<img src="./Assets/x.png" alt="">`;
+        botSymbol = `<img src="./Assets/circle.png" alt="">`;
+    }else{
+        botSymbol = `<img src="./Assets/x.png" alt="">`;
+        userSymbol = `<img src="./Assets/circle.png" alt="">`;
+    }
+    const table = document.querySelector('table');
+    const rows = table.querySelectorAll('tr');
+    for(var i = 0; i<rows.length;i++){
+        const tds = rows[i].querySelectorAll('td');
+        for(var j = 0;j<tds.length;j++){
+            tds[j].id = 'td-'+i+'-'+j;
+            tds[j].innerHTML = '';
+            tds[j].onclick = ((cell) => {
+                console.log(cell.srcElement.id);
+                cellClicked(cell.srcElement.id);
+            });
         }
-        resolve();
-    });
-}
-function rowColExtractionFromId(id){
-    return new Promise(resolve => {
-        let datas = id.split('-');
-        resolve([datas[1], datas[2]]);
-    })
+    }
 }
 async function cellClicked(id){
-    var rowcol;
-    await rowColExtractionFromId(id).then(res => {
-        rowcol = res;
-    }).catch(err => {
-        console.log(err);
-    });
-    if(await isPositionValid(+rowcol[0], +rowcol[1], 'x')){
-        document.getElementById(id).innerHTML = x_mark;
-        console.log(board);
-        if(await isGameOver('x')){
-            // document.getElementById(id).innerHTML = x_mark;
-            alert("Yayy!, you won");
-            return;
-        }
-    }else{
-        return;
-    }
-    await itsComputerMove(o_mark, id);
-    console.log(board);
-    if(await isGameOver('o')){
-        // document.getElementById(id).innerHTML = o_mark;
-        alert("Hahah I won!");
-        return;
-    }
+    console.log('confirm, he clicked!');
 }
